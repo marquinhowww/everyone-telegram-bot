22b5b81385ff3ef48e2372bfca0e36a3dbb6731f
marcos <eptaccio@gmail.com>
2020-06-05 16:52:35 -0300

    simple refactor to use env var

--- commands/everyone.js
+++ commands/everyone.js
@@ -1,14 +1,10 @@
 const { Extra } = require('telegraf')
 
-const ALL_USERS = process.env.ALL_USERS.split(',')
-
 const use = bot => {
   bot.hears([
     /@everyoneqbdbot/i, /@geral/i, /@atencao/i
   ], ctx => {
-
-    const formatedUsers = ALL_USERS.users.join(' ')
-    const message = `${formatedUsers} ${finalGroup.tag}`
+    const message = `${ALL_USERS} #salvegeral`
 
     return ctx.reply(
       message,
